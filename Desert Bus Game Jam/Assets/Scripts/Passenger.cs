a9d7ab9c95ebf8a8c640ae87c11f116dc6160cc8
Michael Kish <mikish123@hotmail.com>
2025-11-18 15:17:10 -0600

    Stuff maybe

--- Desert Bus Game Jam/Assets/Scripts/Passenger.cs
+++ Desert Bus Game Jam/Assets/Scripts/Passenger.cs
@@ -4,22 +4,22 @@ public class Passenger : MonoBehaviour
 {
     [SerializeField] private GameObject destination;
     Vector3 destinationLocation;
-    private float timeOnBus;
+    private float timeRemaining;
     private bool onBus;
 
 
     private void Awake()
     {
         onBus = false;
+        timeRemaining = 60;
         destinationLocation = destination.transform.position;
-        Debug.Log(destinationLocation.ToString());
     }
 
     private void Update()
     {
-        if (onBus)
+        if (onBus && timeRemaining > 0)
         {
-            timeOnBus += Time.deltaTime;
+            timeRemaining -= Time.deltaTime;
         }
     }
 
