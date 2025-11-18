9156389640b5f9a409cb5433b3f10dd29ef541d4
Michael Kish <mikish123@hotmail.com>
2025-11-18 15:53:47 -0600

    Add delayed pick up and drop off

--- Desert Bus Game Jam/Assets/Scripts/BusStop.cs
+++ Desert Bus Game Jam/Assets/Scripts/BusStop.cs
@@ -6,22 +6,31 @@ public class BusStop : MonoBehaviour
     [SerializeField] private List<GameObject> passengers;
     private PlayerBusController playerBus;
     private float maxBusMagnitude = 0.003f;
+    private float timer = 0;
 
+
+    private void OnTriggerExit(Collider other)
+    {
+        //reset timer when exiting zone
+        timer = 0;
+    }
 
     private void OnTriggerStay(Collider other)
     {
         playerBus = other.gameObject.GetComponentInParent<PlayerBusController>();
         float busMagnitude = Mathf.Abs(playerBus.busVelocity());
+        timer += Time.deltaTime;
 
 
         // Check if player is moving too fast
         if (busMagnitude > maxBusMagnitude)
         {
+            timer = 0;
             return;
         }
         
         // Check if there are any passengers to pick up and if the bus is full
-        if (passengers.Count > 0 && playerBus.isBusFull())
+        if (passengers.Count > 0 && playerBus.isBusFull() && timer >= 1)
         {
             Debug.Log("pick up");
 
@@ -29,6 +38,7 @@ public class BusStop : MonoBehaviour
             GameObject passenger = passengers[0];
             passengers.RemoveAt(0);
             playerBus.getPassengers(passenger);
+            timer = 0;
         }
     }
 
--- Desert Bus Game Jam/Assets/Scripts/passengerDropOff.cs
+++ Desert Bus Game Jam/Assets/Scripts/passengerDropOff.cs
@@ -7,6 +7,14 @@ public class passengerDropOff : MonoBehaviour
     private PlayerBusController playerBus;
     private List<GameObject> passengers;
     private float maxBusMagnitude = 0.003f;
+    private float timer = 0;
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        //reset timer when exiting zone
+        timer = 0;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,27 +25,31 @@ public class passengerDropOff : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         float busMagnitude = Mathf.Abs(playerBus.busVelocity());
+        timer += Time.deltaTime;
 
         // Check if player is moving too fast
         if (busMagnitude > maxBusMagnitude)
         {
+            timer = 0;
             return;
         }
-
-        for (int i = 0; i < passengers.Count; i++)
-        {
-            GameObject passenger = passengers[i];
-            Passenger passengerScript = passenger.GetComponent<Passenger>();
-            Vector3 passengerDestinationVector = passengerScript.GetDestinationVector();
-
-            // Check passengers distance from desired destination
-            float distanceFromDestination = Vector3.Distance(this.transform.position, passengerDestinationVector);
-
-            // Remove passenger if at destination
-            if (distanceFromDestination < 5)
+        if (timer >= 1) {
+            for (int i = 0; i < passengers.Count; i++)
             {
-                passengers.RemoveAt(i);
-                Destroy(passenger);
+                GameObject passenger = passengers[i];
+                Passenger passengerScript = passenger.GetComponent<Passenger>();
+                Vector3 passengerDestinationVector = passengerScript.GetDestinationVector();
+
+                // Check passengers distance from desired destination
+                float distanceFromDestination = Vector3.Distance(this.transform.position, passengerDestinationVector);
+
+                // Remove passenger if at destination
+                if (distanceFromDestination < 5)
+                {
+                    passengers.RemoveAt(i);
+                    Destroy(passenger);
+                    timer = 0;
+                }
             }
         }
 
